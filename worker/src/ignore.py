904d62357ec6e13a65d06abaed4b6fb957802ddc
Linex <26711215+linex-cd@users.noreply.github.com>
2020-11-03 23:23:02 +0800

    add ignore port

---
+++ worker/src/ignore.py
@@ -0,0 +1,33 @@
+
+def run(filename):
+
+	print('run: %s' % filename)
+	result = {}
+	result['input'] = filename
+	result['data'] = 'ignore'
+	
+	return result
+
+#enddef
+
+
+if __name__ == "__main__":
+
+	pass
+#enddef
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
