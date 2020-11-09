dc9796ce5141b7ac0fa1dd980ad61132c3c6a376
Linex <26711215+linex-cd@users.noreply.github.com>
2020-11-09 17:51:14 +0800

    Update pdf2rect.py

--- worker/src/port/pdfcore/pdf2rect.py
+++ worker/src/port/pdfcore/pdf2rect.py
@@ -73,6 +73,38 @@ def get_image_with_rect(pdf_file, rect, pagenum):
 
 #enddef	
 
+
+def get_image_with_pagenum(pdf_file, pagenum):
+	
+	doc = fitz.open(pdf_file)
+	page = doc[0];
+
+	tmpfile = "%s-%i.png" % (pdf_file, page.number)
+	
+	
+	# 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像。
+	zoom_x = 1.33333333 #(1.33333333-->1056x816)   (2-->1584x1224)
+	zoom_y = 1.33333333
+	
+	# 此处若是不做设置，默认图片大小为：792X612, dpi=96
+	#2480 3509
+	
+	zoom_y = 3500/800
+	zoom_x = zoom_y
+	mat = fitz.Matrix(zoom_x, zoom_y)
+	
+	pix = page.getPixmap(matrix = mat, alpha = False)  # render page to an image
+	pix.writePNG(tmpfile)
+
+	img = cv2.imread(tmpfile)
+	
+	os.remove(tmpfile)
+	
+	return img;
+		
+
+#enddef	
+
 if __name__ == '__main__':
 	
 	'''
@@ -91,7 +123,7 @@ if __name__ == '__main__':
 	cv2.waitKey(0)
 	
 	
-	img = get_image_with_rect("test3.pdf",  [350,450,550,600], 0)
+	img = get_image_with_pagenum("test3.pdf", -1)
 	
 	cv2.imshow("img",img)
 	cv2.waitKey(0)
