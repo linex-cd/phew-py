219193bc01bcc3708dd99925422936cb02fbc9fb
Linex <linex.cd@qq.com>
2021-01-11 09:06:55 +0800

    remove requirements

--- build_docker_image/build_docker_image.sh
+++ build_docker_image/build_docker_image.sh
@@ -4,7 +4,7 @@
 docker pull python:3.6
 
 #-----------------------------
-version=$(cat ../deploy/VERSION)
+version=$(cat ./VERSION)
 
 #build image
 docker build -f ./BuildImageDockerfile -t phew_image:${version} .
